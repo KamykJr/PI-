9e686ed8ec71656d9174310e959ac452a932eed9
Adam Kaminski <you@example.comkamykjr@pi.com>
2021-12-09 11:51:21 +0100

    3 commit

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import RPi.GPIO as GPIO
 import time
+import os
 #Ustawienia numeracji PIN
 GPIO.setmode(GPIO.BOARD) 
 #Ala ma kota i psa
 #Ustawienie jako wejscie
 GPIO.setup(11, GPIO.IN)
 
+
+
 while True: 
+    var=GPIO.input(11)
+    if var:
+    print 1 > /sys/class/leds/led0/brightness
     print (GPIO.input(11))
-    time.sleep(0.5)
+
+    else: 
+    print 0 > /sys/class/leds/led0/brightness
+    
